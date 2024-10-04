fe1657553a579a36438dc8c22227e691932dca9f
AryanRajTalekar <156109321+AryanRajTalekar@users.noreply.github.com>
2024-10-04 18:41:02 +0530

    1st vid half mark

--- frontend/src/shared/SearchBar.jsx
+++ frontend/src/shared/SearchBar.jsx
@@ -1,32 +1,55 @@
 import React from 'react'
-import './serach-bar.css'
+import { useRef } from 'react'
+import './search-bar.css'
 import {Col,Form,FormGroup} from 'reactstrap'
 
 const SearchBar = () => {
+
+    const locationRef = useRef('')
+    const distanceRef = useRef(0)
+    const maxGroupSizeRef = useRef(0)
+    
+
+    const searchHandler =()=>{
+
+        const location = locationRef.current.value
+        const distance = distanceRef.current.value
+        const maxGroupSize = maxGroupSizeRef.current.value
+
+        if(location==='' || distance==='' || maxGroupSize===''){
+            return alert('All fields are required!!!');
+        }
+
+    }
+
   return <Col lg='12'>
     <div className="search__bar">
         <Form className="d-flex align-items-center gap-4">
-            <FormGroup className="d-flex gap-3 form__group form__group fast">
+            <FormGroup className="d-flex gap-3 form__group form__group-fast">
                 <span><i class="ri-map-pin-line"></i></span>
                 <div className="">
                     <h6>Location</h6>
-                    <input type="text" placeholder='where are you going?' />
+                    <input type="text" placeholder='where are you going?' ref={locationRef} />
                 </div>
             </FormGroup>
-            <FormGroup className="d-flex gap-3 form__group form__group last">
+            <FormGroup className="d-flex gap-3 form__group form__group-fast">
                 <span><i class="ri-map-pin-time-line"></i></span>
                 <div className="">
                     <h6>Distance</h6>
-                    <input type="text" placeholder='Distance K/m' />
+                    <input type="text" placeholder='Distance K/m'  ref={distanceRef}/>
                 </div>
             </FormGroup>
-            <FormGroup className="d-flex gap-3 form__group form__group last">
+            <FormGroup className="d-flex gap-3 form__group form__group-last">
                 <span><i class="ri-group-line"></i></span>
                 <div className="">
                     <h6>Max people</h6>
-                    <input type="number" placeholder='0' />
+                    <input type="number" placeholder='0' ref={maxGroupSizeRef} />
                 </div>
             </FormGroup>
+
+            <span className='search__icon' type="submit" onClick={searchHandler}>
+                <i class="ri-search-line"></i>
+                </span>
         </Form>
     </div>
   </Col>
